d6c492d0da7d61d28f0e28f66203ba0919308177
Tofut <martirosov.davidka@mail.ru>
2022-02-02 19:45:49 +0300

    начал войну

---
+++ LW1/task1.rb
@@ -0,0 +1,21 @@
+puts "Hello world!"
+
+puts "Привет, #{ARGV[0]}!"
+# puts "#{String.methods}"
+
+puts "Какой у тебя любимый язык, #{ARGV[0]}?"
+answer = STDIN.gets.chomp
+if ["руби", "ruby"].include? answer.downcase
+    puts "Nice!"
+else
+    puts "U suck!"
+end
+
+print "Введите команду ОС: "
+os_command = STDIN.gets.chomp
+system os_command
+
+print "Введите команду Ruby: "
+ruby_command = STDIN.gets.chomp
+system "ruby -e \"#{ruby_command}\""
+# ruby -e "puts (5 + 5).to_i.even?" 
