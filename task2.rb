43f28010f6800fbc5ef96982adfce0dcd9319186
Tofut <martirosov.davidka@mail.ru>
2022-02-09 02:16:22 +0300

    2.2

--- task2.rb
+++ task2.rb
@@ -1,2 +1,16 @@
-puts "Сумма цифр числа = #{ARGV[0].to_i.digits.sum}"
+# puts "Сумма цифр числа = #{ARGV[0].to_i.digits.sum}"
+def sum_of_digits(num)
+    num.digits.sum
+end
 
+def max_digit(num)
+    num.digits.max
+end
+
+def min_digit(num)
+    num.digits.min
+end
+
+def product(num)
+    num.digits.reduce(:*)
+end
