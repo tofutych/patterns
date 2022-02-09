eefe9bdcae1be0458ed73c95d4c98e94154bd759
Tofut <martirosov.davidka@mail.ru>
2022-02-09 14:36:32 +0300

    task5-6

--- LW1/task5.rb
+++ LW1/task5.rb
@@ -8,3 +8,22 @@ def task9(str)
     latin_lower = str.split("").select {|char| char.ord > 96 && char.ord < 123 }.join
     latin_lower == latin_lower.reverse
 end
+
+# 18 Найти в тексте даты формата «день.месяц.год».
+def task18(str)
+    contenders = str.scan(%r"[0-9]*[.][0-9]*[.][0-9]*").map {|contender| contender.split(".")}
+    result = []
+    for contender in contenders
+        if contender[0].to_i > 0 && contender[0].to_i < 32
+            if contender[1].to_i > 0 && contender[1].to_i < 13
+                if contender[2].to_i > 0
+                    unless contender[2].start_with?("0")
+                        result.append(contender)
+                    end
+                end
+            end
+        end
+    end
+
+    result
+end
