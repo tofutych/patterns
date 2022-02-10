9cc7d68f0b6b007dbfdba915c2f166a46de192a7
Tofut <martirosov.davidka@mail.ru>
2022-02-10 03:03:24 +0300

    ладно, уже лучше

--- LW2/Department.rb
+++ LW2/Department.rb
@@ -1,11 +1,11 @@
 class Department
-    attr_accessor :name, :phone_number, :duties, :selected_duty
+    attr_accessor :name, :phone_number
     
     def initialize(name, phone_number, duties=Hash.new, selected_duty=nil)
         self.name= name
-        self.phone_number= phone_number
-        self.duties= duties
-        self.selected_duty = selected_duty
+        self.phone_number= Department.correct_phone_number(phone_number)
+        @duties = duties
+        @selected_duty = selected_duty
     end
 
     def to_s
@@ -13,27 +13,48 @@ class Department
     end
 
     def add_duty(duty, description)
-        self.duties[duty] = description
+        @duties[duty] = description
     end
 
     def select_duty(duty)
-        self.selected_duty = duty
+        @selected_duty = duty
     end
 
     def get_description()
-        duties[self.selected_duty]
+        @duties[@selected_duty]
     end
 
     def change_description(description)
-        duties[self.selected_duty] = description
+        @duties[@selected_duty] = description
+    end
+
+    def delete_duty()
+        @duties.delete(@selected_duty)
+    end
+
+    def self.is_ru_phone_number?(phone)
+        if (/^((\+7|7|8)+([0-9]){10})$/).match(phone) == nil
+            return false
+        end
+        true
+    end
+
+    def self.correct_phone_number(phone)
+        if self.is_ru_phone_number?(phone)
+            nums = phone.chars.map{|symb| symb if symb =~ /[0-9]/}.join
+            return "7-" + nums[1..3] + "-" + nums[4..]
+        else
+            raise ArgumentError ,"Invalid phone number."
+        end
     end
 end
 
-murderers = Department.new("Murderers", "+123-45-67-890")
-valve.add_duty("Убить влада новикова", "Быстро и незаметно")
-valve.add_duty("Убить влада ручкина", "Как можно мучительнее")
-valve.select_duty("Убить влада новикова")
-print valve.get_description
-puts
-valve.change_description("Отмена - oн исправился")
-print valve.get_description
+# murderers = Department.new("Murderers", "89186824834")
+# p murderers.phone_number
+# murderers.add_duty("Убить влада новикова", "Быстро и незаметно")
+# murderers.add_duty("Убить влада ручкина", "Как можно мучительнее")
+# murderers.select_duty("Убить влада новикова")
+# p murderers.get_description
+# puts
+# murderers.delete_duty
+# p murderers.get_description
